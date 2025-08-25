80aee50df13ddc7576ade6ea3f29ade89b4fcd4d
Mohammad Mushfiqur Rahman <heemel.mushfiqur@gmail.com>
2025-08-25 22:28:16 +0600

    dependency update

--- menstrual_cycle_predictor/urls.py
+++ menstrual_cycle_predictor/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("predictor/", include("predictor.urls")),
+    path("", include("predictor.urls")),
 ]
